917967c419348b4c04686d224571189bdab29f52
Snehanshu Phukon <hello@snehanshu.com>
2025-01-23 17:24:26 +0530

    browse pincodes

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
@@ -53,6 +54,29 @@ func (s *Server) mountRoutes(app *fiber.App) {
 		return c.Render("views/index", fiber.Map{"Pincodes": pincodes})
 	})
 
+	app.Get("/pincodes", func(c *fiber.Ctx) error {
+		page, limit := c.QueryInt("page", 1), c.QueryInt("limit", 20)
+		if page < 1 {
+			page = 1
+		}
+		if limit < 1 {
+			limit = 1
+		}
+		offset := (page - 1) * limit
+
+		pincodes, err := s.Queries.GetPincodes(c.Context(), queries.GetPincodesParams{
+			Limit: int32(limit), Offset: int32(offset),
+		})
+		if err != nil {
+			s.Logger.Errorw("failed to get pincodes", "error", err)
+			return c.Status(http.StatusInternalServerError).SendString("failed to get pincodes")
+		}
+
+		return c.Render("views/pincodes", fiber.Map{
+			"Pincodes": pincodes, "Limit": limit, "Page": page, "NextPage": page + 1, "PrevPage": page - 1,
+		})
+	})
+
 	app.Get("/pincode/:pincode", func(c *fiber.Ctx) error {
 		var pincode pgtype.Int4
 		if pint, err := strconv.ParseInt(c.Params("pincode"), 10, 32); err != nil {
@@ -72,9 +96,21 @@ func (s *Server) mountRoutes(app *fiber.App) {
 			return c.Status(http.StatusNotFound).SendString("pincode not found")
 		}
 
+		// Find all unique states, in 99.99% cases, there will be only one state
+		statesMap := make(map[string]bool)
+		states := make([]string, 0, len(pincodeResult))
+		for _, p := range pincodeResult {
+			if statesMap[p.Statename.String] {
+				continue
+			}
+			statesMap[p.Statename.String] = true
+			states = append(states, p.Statename.String)
+		}
+
 		return c.Render("views/pincode", fiber.Map{
 			"PostOffices": pincodeResult,
 			"Pincode":     pincode,
+			"State":       strings.Join(states, ", "),
 		})
 	})
 }
